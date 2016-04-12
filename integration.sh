76d2a8639634d1bdc20190126bfe01e6946a7179
Atila Neves <atila.neves@gmail.com>
2016-04-12 10:39:36 +0200

    New integration script

--- integration.sh
+++ integration.sh
@@ -1,6 +1,4 @@
 dub build
 ./parsemk src/phobos.mk > reggaefile.d
-#dub run reggae -- -b binary
-reggae -b binary
-#dmd -I~/.dub/packages/reggae-master/payload -c reggaefile.d
+reggae -b binary -d DMD=dmd
 ./build
